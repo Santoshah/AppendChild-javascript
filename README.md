f588de30a0279d3bb629c6d6f6f6c0cdc7bcb579
Santosh <cssmaniaa@gmail.com>
2017-01-05 18:25:57 +0530

    append child with node javascript

---
+++ README.md
@@ -0,0 +1,1 @@
+# Gulp daily uses node modules. 
---
+++ build/js/main.js
@@ -0,0 +1,50 @@
+
+document.getElementById('user_input').focus();
+function onPress_ENTER()
+{
+	var keyPressed = event.keyCode || event.which;
+
+			        //if ENTER is pressed
+			        if(keyPressed==13)
+			        {
+			        	incre();
+			        	keyPressed=null;
+			        }
+			        else
+			        {
+			        	return false;
+			        }
+			    }
+
+			    var count = 0;
+
+			    onPress_ENTER();
+			    function incre(){
+			    	
+			    	
+			    	count += 1;
+			    	var text = document.createTextNode(count);
+
+			    	var el = document.createElement("li");
+	//get text from input box and create node
+	var user_input = document.getElementById('user_input').value;
+	var user_input_node = document.createTextNode(user_input);
+	
+	//create element node span and add user input inside span
+	var user_el = document.createElement('span');
+	user_el.appendChild(user_input_node);
+
+	//id of list item element
+	var id_el = document.getElementById('list_item');
+
+	//append counter inside the li
+	el.appendChild(text);
+	el.appendChild(user_el);
+
+	id_el.appendChild(el);
+	document.getElementById('user_input').value = " ";
+	document.getElementById('user_input').focus();
+
+}
+
+
